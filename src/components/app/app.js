d5162f5c5f27e41195abc396a66168eae46da9cf
SHOX <theshoxjaxon@gmail.com>
2022-05-03 22:28:13 +0500

    employees for work lists

---
+++ src/components/app/app.js
@@ -0,0 +1,30 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+  let data = [
+    {name: "Shox" , salary: 3700 , increase:false},
+    {name: "Sator" , salary: 6900 , increase:false},
+    {name: "Shawn" , salary: 5100 , increase:false}
+  ];
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,18 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import './employees-list.css';
+
+const EmployeesList = ({data}) => {
+    let elements = data.map(item => {
+        return (
+            <EmployeesListItem name={item.name} salary={item.salary}/>
+        )
+    })
+    return (
+        <ul className="app-list list-group">
+            {elements};
+        </ul>
+    );
+}
+
+export default EmployeesList;
