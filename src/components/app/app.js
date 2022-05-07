b0a6a2b0a6074c854a3c6b6df379a80eb1f6eea1
shox <theshoxjaxon@gmail.com>
2022-05-08 00:56:57 +0500

    deploy delete components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,53 @@
+// ! IMPORTED cOMPONENT FROM REACT
+import { Component } from "react/cjs/react.production.min";
+// ! IMPORTED FROM MY COMPONENTS FOLDER
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
-
+// ! IMPORTED STYLES
 import "./app.css";
 
-function App() {
-  let data = [
-    { name: "Shox", salary: 3700, increase: true, id: 1 },
-    { name: "Sator", salary: 6900, increase: true, id: 2 },
-    { name: "Shawn", salary: 5100, increase: true, id: 3 },
-  ];
-  return (
-    <div className="app">
-      <AppInfo />
+class App extends Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      data: [
+        { name: "Shox", salary: 3700, increase: true, id: 1 },
+        { name: "Sator", salary: 6900, increase: true, id: 2 },
+        { name: "Shawn", salary: 5100, increase: true, id: 3 },
+      ],
+    };
+  }
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      // const index = data.findIndex((elem) => elem.id === id);
+      // const before = data.slice(0, index);
+      // const after = data.slice(index + 1);
+      // const newArr = [...before, ...after];
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeesList data={data} onDelete={this.deleteItem} />
+        <EmployeesAddForm />
       </div>
-
-      <EmployeesList data={data} onDelete={(id) => console.log(id)} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
